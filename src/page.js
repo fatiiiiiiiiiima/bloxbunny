318eb3db49e466e2e574c24e95820104d006835a
Fatima Suleman <160717230+fatiiiiiiiiiima@users.noreply.github.com>
2025-07-24 18:09:51 +0500

    Add files via upload

---
+++ src/page.js
@@ -0,0 +1,10 @@
+import Layout from '../components/layout/layout';
+
+export default function HomePage() {
+  return (
+    <Layout>
+      <h1>Settings</h1>
+      {/* Dashboard content here */}
+    </Layout>
+  );
+}
